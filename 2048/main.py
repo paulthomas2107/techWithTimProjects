adb3b8010f53f9d24b6343c6714487fe1f750167
paulthomas2107 <paulthomas2107@gmail.com>
2024-04-10 13:32:15 +0100

    More

---
+++ 2048/main.py
@@ -0,0 +1,19 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+FPS = 60
+
+WIDTH, HEIGHT = 800, 800
+ROWS = 4
+COLS = 4
+
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
+
+OUTLINE_COLOR = (187, 173, 160)
+OUTLINE_THICKNESS = 10
+BACKGROUND_COLOR = (205, 192, 180)
+FONT_COLR = (119, 110, 101)
